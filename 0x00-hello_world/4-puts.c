169220228e9a1933dba06059eefebe96ec92a8ca
Derajsp <derajsp@gmail.com>
2022-09-09 22:23:37 -0700

    d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-/*obligatory description for main*/
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-puts( "Programming is like building a multiliingual puzzle\n");
-	return(0);
+puts("Programming is like building a multiliingual puzzle\n");
+	return (0);
 }
