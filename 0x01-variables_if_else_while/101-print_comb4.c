0d9bbc107e648246919e4245ab3292fe43ca8b2e
DeraJSP <110997464+DeraJSP@users.noreply.github.com>
2022-09-13 00:43:29 +0100

    Create 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,47 +1,47 @@
 #include <stdio.h>
 
 /**
- *  * main - print triple combos
- *   *
- *    * Description: print triple digit combos
- *     *
- *      * Return: Always 0 (Success)
- *       */
+ * main - print triple combos
+ *
+ * Description: print triple digit combos
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-		int i, j, k;
+	int i, j, k;
 
-			i = 48;
-				j = 48;
-					k = 48;
+	i = 48;
+	j = 48;
+	k = 48;
 
-						while (i < 58)
-								{
-											j = i + 1;
-													while (j < 58)
-																{
-																				k = j + 1;
-																								while (k < 58)
-																													{
-																																			putchar(i);
-																																								putchar(j);
-																																													putchar(k);
-																																																		if (i < 55 || j < 56 || k < 57)
-																																																								{
-																																																															putchar(44);
-																																																																					putchar(32);
-																																																																										}
-																																																							k++;
-																																																											}
+	while (i < 58)
+	{
+		j = i + 1;
+		while (j < 58)
+		{
+			k = j + 1;
+				while (k < 58)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i < 55 || j < 56 || k < 57)
+					{
+						putchar(44);
+						putchar(32);
+					}
+					k++;
+				}
 
-																											j++;
-																													}
+			j++;
+		}
 
-															i++;
-																}
+		i++;
+	}
 
-							putchar(10);
+	putchar(10);
 
-								return (0);
+	return (0);
 }
